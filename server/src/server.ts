c2d3ad839ee8ef93f6b7a7ddc1cda91471ed967c
Laila Silva <laila.cristine@outlook.com>
2022-09-16 00:39:58 -0300

    refactor(server): creating routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,25 @@
-import express, { response } from 'express'
+import express from 'express'
 
 const app = express()
 
-// adicionando rota /ads
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
   return response.json([
-    {
-      id: 1, name: 'Anuncio 1'
-    },
-    {
-      id: 1, name: 'Anuncio 2'
-    },
-    {
-      id: 1, name: 'Anuncio 6'
-    }
+    { id: 1, name: 'Game 1' },
+    { id: 2, name: 'Game 2' },
+    { id: 3, name: 'Game 3' },
   ])
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+  return response.json([])
 })
 
 // porta para acessar a aplicação em desenvolvimento
